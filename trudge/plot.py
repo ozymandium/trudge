768020e5bbb02334c634d7e223a0f0db1e839ef6
Robert Cofield <ozymandium@protonmail.com>
2023-07-15 22:21:06 -0500

    add space between successive days

--- trudge/plot.py
+++ trudge/plot.py
@@ -1,5 +1,6 @@
 # pip
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import pandas as pd
 import numpy as np
 
@@ -25,30 +26,47 @@ def plot_orm(record: pd.DataFrame, set_orms: pd.Series, name: str) -> None:
     N = record.shape[0]
 
     # TODO: break between workouts
-    x_data = np.arange(N)
+    zero_idx = record.index[0]
+    # what gets plotted
+    x_data = [0, 1]
+    orm_data = [0, set_orms[zero_idx]]
+    weight_data = [0, record["weight"][zero_idx]]
+    rep_data = [0, record["reps"][zero_idx]]
+    # label the date for the first set of each workout
     x_ticks = [0]
-    x_labels = [record["time"][record.index[0]].date().isoformat()]
+    x_labels = [record["time"][zero_idx].date().isoformat()]
     # mask means indexes are no longer a range, so we have to use the index property
     # FIXME: could probably do this inline somehow
-    for i1, i2 in zip(record.index[:-1], record.index[1:]):
+    for zero_idx, (i1, i2) in enumerate(zip(record.index[:-1], record.index[1:])):
         t1 = record["time"][i1]
         t2 = record["time"][i2]
+
         if t1 != t2:
-            x_ticks.append(list(record.index).index(i2))
+            x_ticks.append(len(x_data))
             x_labels.append(t2.date().isoformat())
+
+            x_data.append(x_data[-1] + 1)
+            orm_data.append(0)
+            weight_data.append(0)
+            rep_data.append(0)
+
+        x_data.append(x_data[-1] + 1)
+        orm_data.append(set_orms[i2])
+        weight_data.append(record["weight"][i2])
+        rep_data.append(record["reps"][i2])
 
     fig, axes = plt.subplots(2, 1, sharex=True)
 
     # top plot: lifted weight and equivalent 1rm
     axes[0].bar(
         x_data,
-        set_orms,
+        orm_data,
         label="1RM",
         color="r",
     )
     axes[0].bar(
         x_data,
-        record["weight"],
+        weight_data,
         label="Lift",
     )
     axes[0].set_ylabel(trudge.display.get_header("weight"))
@@ -58,11 +76,11 @@ def plot_orm(record: pd.DataFrame, set_orms: pd.Series, name: str) -> None:
     # bottom plot: number of reps
     axes[1].bar(
         x_data,
-        record["reps"],
+        rep_data,
     )
     axes[1].grid(alpha=0.3)
     axes[1].set_ylabel(trudge.display.get_header("reps"))
     # tick only the first set of each workout
-    axes[1].set_xticks(x_ticks, labels=x_labels, rotation=45)
+    axes[1].set_xticks(x_ticks, labels=x_labels, rotation=60)
 
     plt.show()
