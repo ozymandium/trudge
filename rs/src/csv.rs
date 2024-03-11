6909e4ec5d3304aa3a1b8b51c051a376e74b1081
ozymandium <ozymandium@protonmail.com>
2024-03-11 00:16:49 -0500

    doc

--- rs/src/csv.rs
+++ rs/src/csv.rs
@@ -3,9 +3,9 @@ use polars::prelude::*;
 use std::sync::Arc;
 
 /// The minimum effort level
-const EFFORT_MIN: u8 = 1;
+const EFFORT_MIN: u32 = 1;
 /// The maximum effort level
-const EFFORT_MAX: u8 = 5;
+const EFFORT_MAX: u32 = 5;
 
 lazy_static! {
     static ref SCHEMA: Arc<Schema> = Arc::new(Schema::from_iter(
@@ -29,36 +29,60 @@ lazy_static! {
 }
 
 /// Load the CSV file and validate it.
+///
+/// # Arguments
+///
+/// * `file` - The path to the CSV file
+///
+/// # Returns
+///
+/// A DataFrame with the contents of the CSV file
+///
+/// # Errors
+///
+/// If the CSV file has an invalid schema or the effort column has invalid values
 pub fn load_csv(file: &std::path::Path) -> Result<DataFrame, PolarsError> {
-    CsvReader::from_path(file)?
+    let df = CsvReader::from_path(file)?
         .with_schema(Some(SCHEMA.clone()))
         .has_header(true)
-        .finish()
+        .finish()?;
+    validate_csv(&df).unwrap();
+    Ok(df)
 }
 
-// fn validate_csv(df: &polars::prelude::DataFrame) -> Result<(), Box<dyn std::error::Error>> {
-//     // check the schema
-//     let schema = df.schema();
-//     if schema != SCHEMA {
-//         return Err(format!(
-//             "The CSV file has an invalid schema: expected {:?}, got {:?}",
-//             SCHEMA, schema
-//         )
-//         .into());
-//     }
+/// Validate the CSV file.
+///
+/// # Arguments
+///
+/// * `df` - The DataFrame with the contents of the CSV file
+///
+/// # Returns
+///
+/// An empty result if the CSV file is valid
+///
+/// # Errors
+///
+/// If the CSV file has an invalid schema or the effort column has invalid values
+///
+/// # Panics
+///
+/// If the effort column is not found
+fn validate_csv(df: &polars::prelude::DataFrame) -> Result<(), Box<dyn std::error::Error>> {
+    // clone the schema arc
+    let expected_schema = SCHEMA.clone();
 
-//     // check the values in the effort column
-//     let effort = df.column("effort").unwrap().u8().unwrap();
-//     if effort.min()? < EFFORT_MIN || effort.max()? > EFFORT_MAX {
-//         return Err(format!(
-//             "The effort column has invalid values: expected {}-{}, got {}-{}",
-//             EFFORT_MIN,
-//             EFFORT_MAX,
-//             effort.min()?,
-//             effort.max()?
-//         )
-//         .into());
-//     }
+    // check the values in the effort column are within the expected range
+    let effort = df.column("effort").unwrap().u32()?;
+    // the minimum that was received
+    let effort_min = effort.min().unwrap();
+    let effort_max = effort.max().unwrap();
+    if effort_min < EFFORT_MIN || effort_max > EFFORT_MAX {
+        return Err(format!(
+            "The CSV file has an invalid effort column:\nExpected: {} <= effort <= {}\nGot: {} <= effort <= {}",
+            EFFORT_MIN, EFFORT_MAX, effort_min, effort_max
+        )
+        .into());
+    }
 
-//     Ok(())
-// }
+    Ok(())
+}
